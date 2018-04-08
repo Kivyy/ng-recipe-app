49dc5c070eca1191a838bde3b430d77c032d0d22
Kiv Lo <kivlokw@gmail.com>
2018-04-08 15:12:38 -0400

    add ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,38 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>()
+  recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a test duh', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-    new Recipe('A test recipe 2', 'this is a test duh2', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')];
+    new Recipe('A test recipe',
+    'this is a test duh',
+    'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+    [
+      new Ingredient('Meat', 2),
+      new Ingredient('French Fries', 1)
+    ]),
+    new Recipe('A test recipe 2',
+    'this is a test duh2',
+    'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Burger Patties', 2)
+    ]
+    )
+    ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
       return this.recipes.slice();
     }
 
-
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+      this.slService.addIngredients(ingredients);
+    }
 }
